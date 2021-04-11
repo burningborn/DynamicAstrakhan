ea23d68a6fd9ec01d7c373988162914f026f401e
burningborn <evgenyborn@gmail.com>
2021-04-11 20:51:40 +0400

    Separation done

---
+++ DynamicMemory/Print.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#define tab "\t"
+void Print(int arr[], const int n);
+void Print(int** arr, const int m, const int n);
